9115b7e74626714e8f26510755a1029d4112167c
app-wizard <136203201+app-wizard@users.noreply.github.com>
2023-12-28 21:06:59 +0000

    Update env.py

--- env.py
+++ env.py
@@ -1,4 +1,1 @@
-import os
 
-os.environ.setdefault("DATABASE_URL","postgres://danpxyjg:DHHzrRpP4gB6gsY--pY3ePx5cD1j-gkR@mel.db.elephantsql.com/danpxyjg")
-os.environ.setdefault("SECRET_KEY","django-insecure-$onlv+iv%io==9cw#np5m*0n-07a5d+2=x@19qbront#rpqnk_")
