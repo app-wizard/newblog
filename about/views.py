b5af42b3938cbe32e9247b91525da6b06e3e9ca2
Volodymyr Popov <texnolife@gmail.com>
2024-01-26 23:29:11 +0000

    added comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
+from .forms import CollaborateForm
 from .models import About
 # Create your views here.
 
@@ -9,9 +11,19 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
-
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavor to respond within 2 working days.")
+            
+    collaborate_form = CollaborateForm()
+        
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
